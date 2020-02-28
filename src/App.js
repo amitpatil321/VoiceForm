01bea2211e6c2c52771bf3a6e3dfdc23eb6a270b
Amit <amitpatil321@gmail.com>
2020-02-28 13:42:31 +0530

    final draft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { hot } from "react-hot-loader/root";
-import React, { Component, createRef } from "react";
-import { Button, Row, Col, Form, Input, Icon } from "antd";
+import React, { Component } from "react";
+import { Button, Row, Col, Form, Input, Modal } from "antd";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 import { fields } from "./form.json";
 import "antd/dist/antd.css";
@@ -26,61 +27,77 @@ class App extends Component {
     activeInput: null,
     isRecording: false,
     forceStopped: false,
-    form: []
+    formData: []
   };
 
   componentDidMount() {
-    let inputs = fields.map(each => {
-      return { [each.name]: "" };
+    let inputs = {};
+    fields.forEach(each => {
+      inputs[each.name] = "";
     });
-    this.setState({ form: inputs }, () => console.log(this.state.form));
+    this.setState({ formData: inputs });
   }
 
-  // /* Find custom command */
-  // _matchComamnd = command => {
-  //   const { words } = this.state;
-  //   Object.keys(words).forEach(each => {
-  //     if (words[each].includes(command)) {
-  //       return true;
-  //     }
-  //   });
-  //   return false;
-  // };
-
   _proccessCommand = recordedString => {
-    const { activeInput, form } = this.state;
-    // Map command with triggers if any
-    let input = fields.find(each => each.triggers.includes(recordedString));
-    if (input) {
-      // if any active input is there then fill it with value
-      if (activeInput) {
-        const existingValue = this.refs[input.name].props.value;
-        form[activeInput] = form[activeInput] + existingValue;
-        this.setState({ ...form, activeInput: activeInput });
-      } else {
-        console.log(input.name, recordedString);
-        // set input active
-        this.refs[input.name].focus();
-      }
-    } else {
-      // check if command matched with any other custom command
-      switch (recordedString) {
-        case "space":
-          console.log("space");
-          break;
-        case "clear":
-          console.log("clear");
-          break;
-        default:
+    console.log("Recorded String :" + recordedString);
+    let { activeInput, formData } = this.state;
+    // check if command matched with any other custom command
+    switch (recordedString) {
+      // Appends space at the end of input
+      case "space":
+        if (activeInput) {
+          const currValue = this.refs[activeInput].props.value + " ";
+          formData[activeInput] = currValue;
+          this.setState({ ...formData });
+        }
+        break;
+      case "clear":
+      case "clear input":
+      case "clear field":
+      case "start again":
+        if (activeInput) {
+          formData[activeInput] = "";
+          this.setState({ ...formData });
+        }
+        break;
+      case "reset":
+      case "reset form":
+      case "clear form":
+        let clearFields = {};
+        fields.map(each => {
+          clearFields[each.name] = "";
+        });
+        this.setState({ formData: clearFields });
+        break;
+      case "go":
+      case "submit":
+      case "submit form":
+        console.log("Submitting form!");
+        this._handleSubmit();
+        break;
+      case "stop recording":
+      case "stop listening":
+        this._stopRecording();
+        break;
+      default: {
+        // Map command with input triggers if any
+        let input = fields.find(each => each.triggers.includes(recordedString));
+        if (input) {
+          // if any active input is there then fill it with value
+          // set input active
+          this.refs[input.name].focus();
+          this.setState({ activeInput: input.name });
+        } else {
+          if (activeInput) {
+            const existingValue = this.refs[activeInput].props.value;
+            formData[activeInput] =
+              (existingValue ? existingValue : "") + recordedString;
+
+            this.setState({ ...formData, activeInput: activeInput });
+          }
+        }
       }
     }
-
-    // console.log(this["reffirst_name"], command);
-
-    // fields.map(each => {
-    //   console.log(each);
-    //   // console.log(this[each.name].current);
-    // });
   };
 
   _startRecording = () => {
@@ -116,66 +133,90 @@ class App extends Component {
 
   // Handle submit button
   _handleSubmit = e => {
-    const { form } = this.state;
-    console.log(form);
+    const { formData } = this.state;
+    console.log(formData);
   };
 
   _handleChange = e => {
-    const { form } = this.state;
-    this.setState({ form: { ...form, [e.target.name]: e.target.value } });
+    const { formData } = this.state;
+    this.setState({
+      formData: { ...formData, [e.target.name]: e.target.value }
+    });
   };
 
   render() {
-    let { isRecording, form } = this.state;
+    let { isRecording, formData } = this.state;
     return (
       <Row>
-        <Col span={2}></Col>
-        <Col span={8}>
-          <br />
-          <br />
-          {!isRecording ? (
-            <Button
-              type="default"
-              icon="audio"
-              size={"small"}
-              onClick={() => this._startRecording()}
-            >
-              Start
-            </Button>
-          ) : (
-            <Button
-              type="danger"
-              icon="stop"
-              size={"small"}
-              onClick={() => this._stopRecording()}
-            >
-              Stop
-            </Button>
-          )}
-          <Form onSubmit={this._handleSubmit} ref={this._refLoginForm}>
-            {fields.map((each, index) => {
-              return (
-                <Form.Item key={index}>
-                  <Input
-                    placeholder={each.placeholder}
-                    // ref={element => {
-                    //   this["ref" + each.name] = element;
-                    // }}
-                    name={each.name}
-                    ref={each.name}
-                    onChange={this._handleChange}
-                    value={form[each.name]}
-                  />
+        <Col span={24}>
+          <Row>
+            <Col span={8}></Col>
+            <Col span={8} className="form">
+              <h2>Registration Form</h2>
+              <Row>
+                <Col span={2}>
+                  {!isRecording ? (
+                    <FaMicrophone
+                      className="recordingIcon on"
+                      onClick={() => this._startRecording()}
+                    />
+                  ) : (
+                    <FaMicrophoneSlash
+                      className="recordingIcon off"
+                      onClick={() => this._stopRecording()}
+                    />
+                  )}
+                </Col>
+                <Col span={22}>
+                  <div
+                    id="recordingAnimation"
+                    className={`${isRecording ? "show" : "hide"}`}
+                  >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                </Col>
+              </Row>
+              {/* <br /> */}
+              <Form onSubmit={this._handleSubmit} ref={this._refLoginForm}>
+                {fields.map((each, index) => {
+                  return (
+                    <Form.Item key={index}>
+                      <Input
+                        placeholder={each.placeholder}
+                        name={each.name}
+                        ref={each.name}
+                        onChange={this._handleChange}
+                        onFocus={() =>
+                          this.setState({ activeInput: each.name })
+                        }
+                        onBlur={() => this.setState({ activeInput: null })}
+                        value={formData[each.name]}
+                      />
+                    </Form.Item>
+                  );
+                })}
+                <Form.Item className="center">
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
                 </Form.Item>
-              );
-            })}
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+              </Form>
+            </Col>
+            <Col span={8}></Col>
+          </Row>
         </Col>
+        <Modal
+          title="Basic Modal"
+          visible={false}
+          // onOk={this.handleOk}
+          // onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
       </Row>
     );
   }
