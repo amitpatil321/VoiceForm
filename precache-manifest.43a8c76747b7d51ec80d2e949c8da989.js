8471cf7198608a181738515d614f33c57f6da690
Amit <amitpatil321@gmail.com>
2020-03-03 18:48:12 +0530

    Updates

---
+++ precache-manifest.43a8c76747b7d51ec80d2e949c8da989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf45bece91d350c2392001049d274f3b",
+    "url": "/VoiceForm/index.html"
+  },
+  {
+    "revision": "475487e55b7e5ecc2bdf",
+    "url": "/VoiceForm/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "50e83b7a9a10b117bfac",
+    "url": "/VoiceForm/static/css/main.e4440c47.chunk.css"
+  },
+  {
+    "revision": "475487e55b7e5ecc2bdf",
+    "url": "/VoiceForm/static/js/2.530153f6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/VoiceForm/static/js/2.530153f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e83b7a9a10b117bfac",
+    "url": "/VoiceForm/static/js/main.75be6932.chunk.js"
+  },
+  {
+    "revision": "174fc6f2fe1b7fc5c4b7",
+    "url": "/VoiceForm/static/js/runtime-main.38392f2d.js"
+  }
+]);
